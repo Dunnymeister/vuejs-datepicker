08ca5c2baba58061e7429b4158b991c34596492a
Matthew Dunlop <wootismana@gmail.com>
2020-08-07 13:19:25 +0800

    Fixing unit tests

--- test/unit/specs/DateInput/DateInput.spec.js
+++ test/unit/specs/DateInput/DateInput.spec.js
@@ -23,8 +23,10 @@ describe('DateInput', () => {
     wrapper.setProps({
       selectedDate: null
     })
-    expect(wrapper.vm.formattedValue).toBeNull()
-    expect(wrapper.find('input').element.value).toEqual('')
+    wrapper.vm.$nextTick(function () {
+      expect(wrapper.vm.formattedValue).toBeNull()
+      expect(wrapper.find('input').element.value).toEqual('')
+    })
   })
 
   it('formats date', () => {
@@ -37,8 +39,11 @@ describe('DateInput', () => {
       selectedDate: new Date(2016, 1, 15),
       format: () => '2016/1/15'
     })
-    expect(wrapper.vm.formattedValue).toEqual('2016/1/15')
-    expect(wrapper.find('input').element.value).toEqual('2016/1/15')
+
+    wrapper.vm.$nextTick(function () {
+      expect(wrapper.vm.formattedValue).toEqual('2016/1/15')
+      expect(wrapper.find('input').element.value).toEqual('2016/1/15')
+    })
   })
 
   it('emits showCalendar', () => {
@@ -50,7 +55,10 @@ describe('DateInput', () => {
     wrapper.setProps({
       bootstrapStyling: true
     })
-    expect(wrapper.find('input').element.classList).toContain('form-control')
+
+    wrapper.vm.$nextTick(function () {
+      expect(wrapper.find('input').element.classList).toContain('form-control')
+    })
   })
 
   it('appends bootstrap classes', () => {
@@ -58,15 +66,21 @@ describe('DateInput', () => {
       inputClass: 'someClass',
       bootstrapStyling: true
     })
-    expect(wrapper.find('input').element.classList).toContain('form-control')
-    expect(wrapper.find('input').element.classList).toContain('someClass')
+
+    wrapper.vm.$nextTick(function () {
+      expect(wrapper.find('input').element.classList).toContain('form-control')
+      expect(wrapper.find('input').element.classList).toContain('someClass')
+    })
   })
 
   it('can be disabled', () => {
     wrapper.setProps({
       disabled: true
     })
-    expect(wrapper.find('input').attributes().disabled).toBeDefined()
+
+    wrapper.vm.$nextTick(function () {
+      expect(wrapper.find('input').attributes().disabled).toBeDefined()
+    })
   })
 
   it('accepts a function as a formatter', () => {
